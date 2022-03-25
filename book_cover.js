8b992f32e6e85fe9ce3558ca66092a7897e5f91a
RX1334 <rxia@princeton.edu>
2022-03-25 03:26:18 -0400

    front + back cover changes

--- book_cover.js
+++ book_cover.js
@@ -2,6 +2,7 @@ var date;
 var category;
 var content;
 var font;
+var fileName;
 
 function generateCover() {
   var dateTime = document.getElementById("Timestamp").value;
@@ -10,6 +11,9 @@ function generateCover() {
 
   category = document.getElementById("Category").value;
   console.log(category);
+
+  fileName = document.getElementById("bookName").value;
+  console.log(fileName);
 
   if (category.localeCompare('Nanum') == 0) {
     font = 'assets/NanumMyeongjo-Bold.ttf';
@@ -35,13 +39,52 @@ function generateCover() {
   // console.log(content);
 
   let myp5 = new p5(bc, document.getElementById("cover"));
+  let myp52 = new p5(bbc, document.getElementById("backCover"));
 }
 
 // set up for instance-based p5js
 const bc = ( sketch ) => {
 
-  let x = 276;
-  let y = 760;
+  let x = 240;
+  let y = 240;
+
+  let DMSans;
+  sketch.preload = () => {
+    DMSans = sketch.loadFont(font);
+  }
+
+  sketch.setup = () => {
+    sketch.createCanvas(1748, 2480);
+    sketch.textFont(DMSans);
+    sketch.textSize(88);
+    sketch.textAlign(sketch.LEFT);
+  };
+
+  sketch.draw = () => {
+    sketch.background(255);
+    sketch.noStroke();
+
+    var myrng = new Math.seedrandom(date.toString());
+
+    for (let i = 0; i < 187; i++) {
+      let col = Math.floor(i / 17);
+      let row = i % 17;
+
+      sketch.fill(Math.floor(255 * myrng()));
+      sketch.ellipse(x + col * 128, y + row * 128, 72);
+    }
+
+    sketch.saveCanvas(fileName + "-front.png");
+    sketch.remove();
+    sketch.noLoop();
+  };
+};
+
+// set up for instance-based p5js
+const bbc = ( sketch ) => {
+
+  let x = 240;
+  let y = 240;
 
   let DMSans;
   sketch.preload = () => {
@@ -58,20 +101,8 @@ const bc = ( sketch ) => {
   sketch.draw = () => {
     sketch.background(255);
     sketch.text(date.toString(), 240, 240, 900);
-    sketch.fill(0);
-    sketch.noStroke();
 
-    var myrng = new Math.seedrandom(date.toString());
-
-    for (let i = 0; i < 72; i++) {
-      let col = Math.floor(i / 12);
-      let row = i % 12;
-
-      sketch.fill(Math.floor(255 * myrng()));
-      sketch.ellipse(x + col * 128, y + row * 128, 72);
-    }
-
-    sketch.saveCanvas('cover.png');
+    sketch.saveCanvas(fileName + "-back.png");
     sketch.remove();
     sketch.noLoop();
   };
